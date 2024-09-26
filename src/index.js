0ac57f00820ece38e0b48ff3bd011c40c24bba3b
MariaKhmel <mariakhmel8@gmail.com>
2024-09-26 03:39:07 +0300

    fx

--- src/index.js
+++ src/index.js
@@ -853,3 +853,40 @@ const movie = {
 // console.log(res);
 // const res2 = arr.forEach((el, index, array) => (array[index] = el * 4));
 // console.log(arr);
+
+// const postToUpdate = {
+//   id: 1,
+//   body: "CRUD is really awesome!",
+// };
+
+// const options = {
+//   method: "PATCH",
+//   body: JSON.stringify(postToUpdate),
+//   headers: {
+//     "Content-Type": "application/json; charset=UTF-8",
+//   },
+// };
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postToUpdate.id}`, options)
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   })
+//   .then(post => console.log(post))
+//   .catch(error => console.log("ERROR:", error));
+
+// const postIdToDelete = 1;
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`, {
+//   method: "DELETE",
+// })
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   })
+//   .then((deletedPost) => console.log(deletedPost))
+//   .catch(error => console.log("Error:", error)); 
