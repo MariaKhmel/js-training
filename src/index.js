3de24ccbe63e494eb2f9e1722463442164ce476c
MariaKhmel <mariakhmel8@gmail.com>
2024-09-28 02:02:20 +0300

    http

--- src/index.js
+++ src/index.js
@@ -889,4 +889,64 @@ const movie = {
 //     return response.json();
 //   })
 //   .then((deletedPost) => console.log(deletedPost))
-//   .catch(error => console.log("Error:", error)); 
+//   .catch(error => console.log("Error:", error));
+
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+
+// const App = () => {
+//   // 1. Оголошуємо стан
+//   const [articles, setArticles] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       const response = await axios.get(
+//         "https://hn.algolia.com/api/v1/search?query=react"
+//       );
+//       // 2. Записуємо дані в стан
+//       setArticles(response.data.hits);
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Latest articles</h1>
+//     </div>
+//   );
+// };
+
+
+// const App = () => {
+//   const [articles, setArticles] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       const response = await axios.get(
+//         "https://hn.algolia.com/api/v1/search?query=react"
+//       );
+//       setArticles(response.data.hits);
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Latest articles</h1>
+
+//       {articles.length > 0 && (
+//         <ul>
+//           {articles.map(({ objectID, url, title }) => (
+//             <li key={objectID}>
+//               <a href={url} target="_blank" rel="noreferrer noopener">
+//                 {title}
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
