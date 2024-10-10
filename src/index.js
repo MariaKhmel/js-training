5b9016f3c36a7183451ea2d2e9b9e976c8ccca50
Maria Khmel <mariakhmel8@gmail.com>
2024-10-11 02:03:56 +0300

    hw

--- src/index.js
+++ src/index.js
@@ -1261,3 +1261,38 @@ const movie = {
 //   .then(response => response.json())
 //   .then(post => console.log(post))
 //   .catch(error => console.log("ERROR" + error));
+
+// const fetchUsersBtn = document.querySelector(".btn");
+// const userList = document.querySelector(".user-list");
+
+// fetchUsersBtn.addEventListener("click", () => {
+//   fetchUsers()
+//     .then((users) => renderUsers(users))
+//     .catch((error) => console.log(error));
+// });
+
+// function fetchUsers() {
+//   return fetch(
+//     "https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name"
+//   ).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+
+// function renderUsers(users) {
+//   const markup = users
+//     .map((user) => {
+//       return `
+//           <li>
+//             <p><b>Name</b>: ${user.name}</p>
+//             <p><b>Email</b>: ${user.email}</p>
+//             <p><b>Company</b>: ${user.company.name}</p>
+//           </li>
+//       `;
+//     })
+//     .join("");
+//   userList.insertAdjacentHTML("beforeend", markup);
+// }
