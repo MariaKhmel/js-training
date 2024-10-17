61899b06cb2dc0b45b6738b460b2d5e79e85e3b4
Maria Khmel <mariakhmel8@gmail.com>
2024-10-17 23:30:43 +0300

    promise

--- src/index.js
+++ src/index.js
@@ -1424,3 +1424,28 @@ const movie = {
 // const intervalId = setInterval(greet, 3000);
 
 // clearInterval(intervalId);
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(5);
+//   }, 2000);
+// });
+
+// promise
+//   .then(value => {
+//     console.log(value); // 5
+//     return value * 2;
+//   })
+//   .then(value => {
+//     console.log(value); // 10
+//     return value * 3;
+//   })
+//   .then(value => {
+//     console.log(value); // 30
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.log("finally");
+//   });
