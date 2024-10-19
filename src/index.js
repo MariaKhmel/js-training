cafd58f09b6f94638dcde1aaf6fcae67e3a082c2
Maria Khmel <mariakhmel8@gmail.com>
2024-10-19 23:48:21 +0300

    sort

--- src/index.js
+++ src/index.js
@@ -1469,3 +1469,16 @@ const movie = {
 //     }, delay);
 //   });
 // };
+
+
+// const students = [
+//   { name: "Mango", score: 83, courses: ["mathematics", "physics"] },
+//   { name: "Poly", score: 59, courses: ["science", "mathematics"] },
+//   { name: "Ajax", score: 37, courses: ["physics", "biology"] },
+//   { name: "Kiwi", score: 94, courses: ["literature", "science"] },
+// ];
+
+// const names = students.toSorted((a, b) => a.score - b.score)
+//   .map(student => student.name);
+
+// console.log(names);
