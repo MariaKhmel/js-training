e9c026656c0d7c677dd9e9ff2e3aeb14f1e61972
Maria Khmel <mariakhmel8@gmail.com>
2024-10-20 23:06:52 +0300

    recusrion

--- src/index.js
+++ src/index.js
@@ -1506,3 +1506,14 @@ const movie = {
 // colorPickerOptions.find((option) => option.label === "blue");
 // colorPickerOptions.find((option) => option.label === "pink");
 // colorPickerOptions.find((option) => option.label === "white");
+
+function factorial(n) {
+  // Base case
+  if (n === 0) {
+    return 1;
+  }
+  // Recursive case
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5)); // Output: 120
