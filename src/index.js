7a7d590677b6d1298ad1524ba7fbc97826cbb520
Maria <mariakhmel8@gmail.com>
2024-10-24 22:30:29 +0300

    promise

--- src/index.js
+++ src/index.js
@@ -1606,3 +1606,14 @@ const movie = {
 //     _sort: "name"
 //   }
 // });
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve(1), 1000);
+// });
+
+// const p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => reject(2), 2000);
+// });
+
+// Promise.race([p1, p2])
+//   .then(value => console.log(value)) // 1
+// 	.catch (error => console.log(error));
