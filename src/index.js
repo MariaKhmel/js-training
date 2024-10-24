3c07a1f2abefe2bc902608ed431ceb1297302b28
Maria <mariakhmel8@gmail.com>
2024-10-24 22:27:31 +0300

    URLSearchParams

--- src/index.js
+++ src/index.js
@@ -1592,3 +1592,11 @@ const movie = {
 //   const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5&_page=3");
 //   console.log("Posts: ", response.data);
 // };
+// const searchParams = new URLSearchParams({
+//   _limit: 5,
+//   _sort: "name",
+// });
+
+// console.log(searchParams.toString()); // "_limit=5&_sort=name"
+// const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
+// console.log(url); // "https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name"
