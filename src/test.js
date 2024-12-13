acec3cf9c0f7d4004fcd8831e23e217212c83d0f
Maria Khmel <mariakhmel8@gmail.com>
2024-12-13 02:26:03 +0200

    switch

--- src/test.js
+++ src/test.js
@@ -58,3 +58,40 @@
 // console.log(secondSymbol('Hello world!!!', 'l'))
 // console.log(secondSymbol('Hello world!!!', 'o'))
 // console.log(secondSymbol('Hello world!!!', 'A'))
+
+// function likes(names) {
+//   switch (names.length) {
+//     case 0:
+//       return "no one likes this";
+//     case 1:
+//       return `${names[0]} likes this`;
+//     case 2:
+//       return `${names[0]} and ${names[1]} like this`;
+//     case 3:
+//       return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//     default:
+//       return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+//   }
+// }
+
+// console.log(likes([]));
+// console.log(likes(['Peter']));
+// console.log(likes(['Jacob', 'Alex']));
+// console.log(likes(['Max', 'John', 'Mark']));
+// console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']));
+
+// weights array - is golden ore pieces weights
+// n - is the weight of figurine
+//
+// return an array describing the left and right scale pans, 
+// prepared to test that the figurine (possibly weighing n) 
+// placed on the right pan afterward will balance the scales
+function weightN(weights, n) {
+
+  // for this example, let's assume there are at least 3 ore pieces
+
+  const left = [weights[0], weights[2]];
+  const right = [weights[1]];
+
+  return [left, right];
+}
