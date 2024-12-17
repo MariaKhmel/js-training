ceb8d48cc52cf1afa01bfcdb52f18b3ac093fb65
Maria Khmel <mariakhmel8@gmail.com>
2024-12-17 04:32:06 +0200

    chaining

--- src/test.js
+++ src/test.js
@@ -121,3 +121,17 @@
 
 // registerGuest("Mango", notify); // "Registering Mango!"
 // // "Dear Mango, your room will be ready in 30 minutes"
+
+// const students = [
+//   { name: "Mango", score: 83, courses: ["mathematics", "physics"] },
+//   { name: "Poly", score: 59, courses: ["science", "mathematics"] },
+//   { name: "Ajax", score: 37, courses: ["physics", "biology"] },
+//   { name: "Kiwi", score: 94, courses: ["literature", "science"] },
+// ];
+
+
+// const uniqueCourses = students.flatMap(student => student.courses)
+//   .filter((course, index, arr) => arr.indexOf(course) === index)
+//   .toSorted((a, b) => a.localeCompare(b));
+
+// console.log(uniqueCourses)
