9c3fefaefda112ad6fe5d1c79689881f7f8aebb8
Maria Khmel <mariakhmel8@gmail.com>
2025-03-18 23:31:17 +0200

    fx

--- src/index.js
+++ src/index.js
@@ -1879,3 +1879,10 @@ const movie = {
 
 // const greetFun = person.greet.bind(person);
 // greetFun()
+document.getElementById("parent").addEventListener("click", () => {
+  console.log("Parent clicked!");
+}, true);  // `true` enables capturing
+
+document.getElementById("child").addEventListener("click", () => {
+  console.log("Child clicked!");
+});
